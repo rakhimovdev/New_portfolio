809379cb1ce8b1d2a41afefd3ed0b6eae7d70eaf
Muhammadali <rahimov.dev1@gmail.com>
2025-01-10 14:41:29 +0500

    Add the media of the Projects

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,8 +2,20 @@ import React from 'react'
 import "./Contact.css"
 function Contact() {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='Conatact'>
+            <form action="">
+                <label htmlFor="">Name</label>
+                <input type="text" placeholder='Name...' />
+                <label htmlFor="">Lastname</label>
+                <input type="text" placeholder='Lastname...' />
+                <label htmlFor="">Phone Number</label>
+                <label className='phone_label' htmlFor="">+998 <input type="number" name="" id="" placeholder='Phone Number...' /></label>
+                <label htmlFor="">Email</label>
+                <input type="email" name="" id="" placeholder='Email...' />
+                <label htmlFor="" >Text</label>
+                <input type="text" placeholder='Text' />
+                <button>SEND</button>
+            </form>
         </div>
     )
 }
