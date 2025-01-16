3236691779fb23514d1755db4e5ea80b9ef9611c
Muhammadali <rahimov.dev1@gmail.com>
2025-01-16 14:14:06 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './pages/About/About';
-import Projects from './pages/Projects/Projects';
-import Contact from './pages/Contact/Contact';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/contact' element={<Contact />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
