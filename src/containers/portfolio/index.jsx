2331c73092d1a96b197c5338fd30ab5381168fc6
Muhammadali <rahimov.dev1@gmail.com>
2026-02-10 17:22:01 +0500

    fix some errors

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -3,9 +3,6 @@ import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
 import ImageOne from "../../images/image1.jpg";
 import ImageTwo from "../../images/image2.jpg";
-import ImageThree from "../../images/image3.jpg";
-import ImageFour from "../../images/image4.jpg";
-import ImageFive from "../../images/image5.jpg";
 import "./styles.scss";
 import { useState } from "react";
 
